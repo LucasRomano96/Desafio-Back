b051aa40006b0d74fac1ae015613c7cde6d5674f
Lucas Romano <lucasromano96@gmail.com>
2023-06-23 18:57:59 -0300

    add mongoose new version

--- src/dao/dbconfig.js
+++ src/dao/dbconfig.js
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
 import config from "../config/config.js"
 
-mongoose.set("strictQuery", false);
-mongoose.connect(config.MONGO_URL, (error) => {
-    if (error) {
-        console.log("Hubo un error al conectar con base de datos");
-    } else {
-        console.log("Conectado a la base de datos con éxito");
-    }
-});
+try {
+    await mongoose.connect(config.MONGO_URL);
+    console.log("Conectado a la base de datos con éxito");
+} catch (error) {
+    console.log(error);
+}
